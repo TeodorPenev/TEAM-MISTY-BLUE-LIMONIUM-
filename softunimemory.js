9f9b60de083db867ca64ae019062870d2107863d
Bebbo <mrbebz@gmail.com>
2014-11-18 00:25:28 +0200

    script - Update
    timer function

--- softunimemory.js
+++ softunimemory.js
@@ -83,7 +83,49 @@ function checkcard( e ) {
   }
 
 }
-  
+//TIMER
+	var timer = (function() {
+    var basePeriod = 1000;
+    var currentSpeed = 1;
+    var timerElement;
+    var timeoutRef;
+	var minutes = 0;
+    var seconds = 0;
+
+    return {
+      start : function(speed, id) {
+        if (speed >= 0) {
+          currentSpeed = speed;
+        }
+        if (id) {
+          timerElement = document.getElementById(id);
+        }
+        timer.run();	
+      },
+
+      run: function() {
+		
+        if (timeoutRef) clearInterval(timeoutRef);
+        if (timerElement) {
+          timerElement.innerHTML = minutes + ':' + seconds;
+        }
+		if(seconds >= 59){
+		seconds = 0;
+		minutes++;
+		}
+        if (currentSpeed) {
+          timeoutRef = setTimeout(timer.run, basePeriod/currentSpeed);
+        }
+        ++seconds;
+      },
+
+      setSpeed: function(speed) {
+        currentSpeed = +speed;
+        timer.run();
+      }
+    }
+
+}());
 function win() {
 
   document.body.className += ' win';
@@ -109,3 +151,5 @@ function clear() {
 }
 
 window.addEventListener ( 'load', init, false );
+window.onload = function(){timer.start(1, 'timer');};
+
